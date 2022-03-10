6ec6a23cefb6ffe29e00fcfea724c6e2e9747618
Rakesh <96762361+Rakesh8864@users.noreply.github.com>
2022-03-10 09:42:50 +0000

    Completed

--- p3final.c
+++ p3final.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+int input_n_and_r(int *n, int *r);
+int ncr(int n, int r);
+void output(int n, int r, int result);
+
+int main() {
+  int n, r, ans;
+  input_n_and_r(&n, &r);
+  ans = ncr(n, r);
+  output(n, r, ans);
+  return 0;
+}
+
+int input_n_and_r(int *n, int *r) {
+  printf("Enter n : ");
+  scanf("%d", n);
+  printf("Enter r : ");
+  scanf("%d", r);
+}
+
+int ncr(int n, int r) {
+  int ans, num = 1, den = 1;
+  
+  for (int i=1; i<=(n-r); i++) {
+    den = den*i;
+  }
+  
+  for (int i=1; i<=r; i++) {
+    den = den*i;
+  }
+  
+  for (int i=1; i<=n; i++) {
+    num = num*i;
+  }
+
+  ans = num/den;
+  return ans;
+}
+
+void output(int n, int r, int result) {
+  printf("%dC%d = %d\n", n, r, result);
+    }
